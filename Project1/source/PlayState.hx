f0d8b835f9117526350e554e0a832b765b77e89b
Staethe <jzmynb@gmail.com>
2016-09-10 14:54:13 -0400

    Reference Player Character

--- Project1/source/PlayState.hx
+++ Project1/source/PlayState.hx
@@ -9,8 +9,12 @@ import flixel.math.FlxMath;
 
 class PlayState extends FlxState
 {
+	var _player:Player;
+	
 	override public function create():Void
 	{
+		_player = new Player(20,20);
+		add(_player);
 		super.create();
 	}
 
