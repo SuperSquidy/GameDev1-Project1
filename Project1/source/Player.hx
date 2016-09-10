b65c3c8384db649ba631ef2675b44cf96e273d1e
Staethe <jzmynb@gmail.com>
2016-09-10 15:11:40 -0400

    Added wasd & arrow keys

--- Project1/source/Player.hx
+++ Project1/source/Player.hx
@@ -10,16 +10,65 @@ import flixel.FlxObject;
 class Player extends FlxSprite
 {
 
+
+	//Helper Variables
+	var speed:Float = 200;
+	var _rot: Float = 0;
+	var _up:Bool = false;
+	var _down:Bool = false;
+	var _left:Bool = false;
+	var _right:Bool = false;
+
 	public function new(?X:Float=0, ?Y:Float=0)
 	{	
 		super(X, Y);
-		loadGraphic("assets/images/tmpPlayer.png", true, 200, 200)
-		
+		makeGraphic(16,16, FlxColor.GREEN);
 		drag.x = drag.y = 1300; //Mild slide when stopping
 	}
 
-	override public function update(elapsed:Float):void
+	override public function update(elapsed:Float):Void
 	{
-		movement	
+		movement();
+		super.update(elapsed);
+	}
+
+	function movement():Void
+	{
+
+		_up = FlxG.keys.anyPressed([UP, W]);
+		_down = FlxG.keys.anyPressed([DOWN, S]);
+		_left = FlxG.keys.anyPressed([LEFT, A]);
+		_right = FlxG.keys.anyPressed([RIGHT, D]);	
+
+		// cancel out opposing directions
+		 if (_up && _down){
+		 	_up = _down = false;
+		 }
+		 if (_left && _right){
+		 	_left = _right = false;
+		 }
+		 
+		 if (_up || _down || _left || _right){
+			 if (_left)
+			 {
+				 _rot = 180;
+				 facing = FlxObject.LEFT;
+				 if (_up) _rot += 45;
+				 else if (_down) _rot -= 45;
+			 }
+			 else if (_right)
+			 {
+				 _rot = 0;
+				 facing = FlxObject.RIGHT;
+				 if (_up) _rot -= 45;
+				 else if (_down) _rot += 45;
+			 }
+			 else if (_down) _rot = 90;
+			 else if (_up) _rot = 270;
+			 
+		 	velocity.set(speed,0);
+			velocity.rotate(new FlxPoint(0,0), _rot);
+		 }
+	
 	}
 }
