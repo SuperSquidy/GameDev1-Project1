14ee168092467ed550fc82e2c7040f41b4aed863
swood456 <crazedsportsfan@gmail.com>
2016-09-10 13:27:47 -0400

    Made a build and added particles
    the particles are in the main menu, very very basic

--- Project1/source/MenuState.hx
+++ Project1/source/MenuState.hx
@@ -6,12 +6,30 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
+import flixel.effects.particles.FlxEmitter;
+import flixel.effects.particles.FlxParticle;
+import flixel.util.FlxColor;
 
 class MenuState extends FlxState
 {
+
+	private var _emitter:FlxEmitter;
+
 	override public function create():Void
 	{
 		super.create();
+
+		_emitter = new FlxEmitter(FlxG.width / 2 , FlxG.height / 2, 200);
+		
+		// All we need to do to start using it is give it some particles. makeParticles() makes this easy!
+		
+		_emitter.makeParticles(2, 2, FlxColor.WHITE, 200);
+
+		add(_emitter);
+
+		_emitter.start(false, 0.01);
+
+
 	}
 
 	override public function update(elapsed:Float):Void
