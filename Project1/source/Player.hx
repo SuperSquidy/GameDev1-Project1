52e9941a344ed0636aa90279b90bc0ce950927d0
Staethe <jzmynb@gmail.com>
2016-09-10 14:04:42 -0400

    Project template

---
+++ Project1/source/Player.hx
@@ -0,0 +1,19 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.math.FlxPoint;
+import flixel.FlxObject;
+
+class Player extends FlxSprite
+{
+
+	public function new(?X:Float=0, ?Y:Float=0)
+	{	
+		
+	}
+
+
+}
