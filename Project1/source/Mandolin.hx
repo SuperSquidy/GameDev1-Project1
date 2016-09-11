6596c7438b8116c0da5e94cd38de7b5b69c7117e
Staethe <jzmynb@gmail.com>
2016-09-10 20:59:13 -0400

    Outline for playNotes() & Header text update

--- Project1/source/Mandolin.hx
+++ Project1/source/Mandolin.hx
@@ -1,14 +1,17 @@
 /*
-This file contains information pertaining to the player character including :
- - Controls : WASD, Arrow Keys
+This file contains information & functions related to the Mandolin instrument, including :
+ - 
+
 
 This file WILL contain :
- - Instrument Controls
- - Character images & animations
- - Character walking sound effects
+
 
 Current Plan:
-	Mandolin will be able to play multiple notes at once
+	- Mandolin will not play multiple notes at once:
+	  When it registers multiple buttons are pressed, it will
+	  play the note with the lowest index in stringsDown
+
+	  Reference playNotes() below
 */
 
 package;
@@ -28,15 +31,18 @@ class Mandolin extends FlxBasic
 	}
 
 	/*
-		@parameters : stringsDown
+		@parameters : stringsDown<Bool>
 			Array of Booleans where each index represents 
 			whether that key is currently being pressed or 
 			not, corresponding to this mapping: [ h, j, k, l, ;]
+
+		@return : returns the index of the note chosen to be played
+		Current mapping can be found in Player.hx under instrumentKeys()
 	*/
-	public function playNotes(stringsDown:Array<Bool>):Void{
+	public function playNotes(stringsDown:Array<Bool>):Int{
 
-
+		//insert switch statement here
+		return 0;
 	}
-
 
 }
