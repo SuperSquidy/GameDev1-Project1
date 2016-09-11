706ec33ac84cc9a0e5087d3509c4324eafcd7026
wrighp <wrightCoder@gmail.com>
2016-09-11 18:49:24 -0400

    Added checkpoint support.

--- Project1/source/WorldState.hx
+++ Project1/source/WorldState.hx
@@ -5,6 +5,7 @@ import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.group.FlxGroup;
+import flixel.math.FlxPoint;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 
@@ -12,8 +13,12 @@ class WorldState extends FlxState
 {
 	public var level:TiledLevel;
 	public var player:Player;
+	public var floors:FlxGroup;
+	public var checkpoints:FlxGroup;
+	
 	
 	private var _levelName:String;
+	private var _checkpointPosition:FlxPoint;
 	
 	//Can pass a player in constructor if a player already exists (for loading multiple levels)
 	public function new(?player:Player, ?levelName:String = "test_map.tmx") 
@@ -26,11 +31,15 @@ class WorldState extends FlxState
 	{
 		super.create();
 		
+		floors = new FlxGroup();
+		checkpoints = new FlxGroup();
 		level = new TiledLevel("assets/tiled/" + _levelName, this);
+		_checkpointPosition = player.getPosition();
 		
 		//Added in order of depth, back to front
 		add(level.backgroundLayer); //add backgrounds
 		add(level.imagesLayer); 	//add static images
+		add(checkpoints);
 		add(level.objectsLayer);	//add objects (including player)
 		add(level.foregroundTiles); //add forground
 		
@@ -38,6 +47,7 @@ class WorldState extends FlxState
 		
 		var backButton  = new FlxButton(20,20, "Back", function(){FlxG.switchState(new MenuState());});
 		add(backButton); //Back to menu button
+		
 	}
 	
 	override public function update(elapsed:Float):Void 
@@ -47,5 +57,27 @@ class WorldState extends FlxState
 		level.collideWithLevel(player);
 		
 		
+		FlxG.overlap(player, checkpoints, onCheckpointCollision);
+		if (FlxG.overlap(player,floors)){
+			//Death by pitfall
+			onDeath();
+		}else if(player.health <=0){
+			//Death by loss of health
+			onDeath();
+		}
+		
+	}
+	private function onCheckpointCollision(Player:FlxObject, Checkpoint:FlxObject):Void{
+		//If this checkpoint wasn't already activated (there may be particle effects or a light or something)
+		if (!Checkpoint.getPosition().equals(_checkpointPosition)){
+			//Activate this checkpoint
+			_checkpointPosition = Checkpoint.getPosition();
+		}
+	}
+	public function onDeath():Void
+	{
+		//Reset Player Health
+		player.health = 1;
+		player.setPosition(_checkpointPosition.x,_checkpointPosition.y);
 	}
 }
