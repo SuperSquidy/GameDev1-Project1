2f04f005be36883820b23c968206773b25a4e221
Staethe <jzmynb@gmail.com>
2016-09-12 14:45:52 -0400

    Initial CheckPoint Class

---
+++ Project1/source/CheckPoint.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.math.FlxPoint;
+import flixel.FlxBasic;
+import flixel.FlxG.sound;
+
+
+class CheckPoint extends FlxSprite
+{
+	
+	
+}
