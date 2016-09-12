a9852247df124adfcb3b26a7f3d03918801f25b3
Staethe <jzmynb@gmail.com>
2016-09-12 14:48:15 -0400

    Moved checkpoint details from TiledLevel into CHeckPoint

--- Project1/source/CheckPoint.hx
+++ Project1/source/CheckPoint.hx
@@ -12,5 +12,15 @@ import flixel.FlxG.sound;
 class CheckPoint extends FlxSprite
 {
 	
+
+	public function new(){
+		super();
 	
+		var checkpoint = new FlxSprite(x, y - 32);
+		var color = new FlxColor();
+		color.setRGB(255, 255, 255, 128);
+		checkpoint.makeGraphic(32, 64, color);
+		state.checkpoints.add(checkpoint);
+
+	}			
 }
