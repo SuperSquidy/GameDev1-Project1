523b1da16a03be399b6d911a4b23b074bc724096
Staethe <jzmynb@gmail.com>
2016-09-18 17:36:45 -0400

    Working on getting text to update

--- Project1/source/WaterShrine.hx
+++ Project1/source/WaterShrine.hx
@@ -1,6 +1,14 @@
 package;
+
 import Shrine;
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.util.FlxColor;
+import flixel.FlxG;
 import flixel.math.FlxPoint;
+import flixel.FlxBasic;
+import flixel.FlxG.sound;
+
 
 /*
 This file contains information pertaining to the Water Shrine including :
@@ -22,12 +30,16 @@ class WaterShrine extends Shrine
 	private static var songLearned:Bool = false;
 	private static var storyLearned:Bool = false;
 	var ticker:TickingText;
+	var playerTrigger:Trigger;
 
 	override private function create():Void{
 		super.create();
+		
 		//Don't worry about offsets for now
 		var tileSizeX = 64;
 		var tileSizeY = 64;
+		
+		//Sprite Sheet & Animations
 		loadGraphic('assets/images/Shrines/Water_Shrine_SpriteSheet.png', true, tileSizeX, tileSizeY);
 		animation.add("idle", [0], 0, false);
 		animation.play("idle", false);
@@ -37,42 +49,44 @@ class WaterShrine extends Shrine
 	}
 	
 	override public function update(elapsed:Float):Void{
-	//	if (getCollisionPlayer())
-	//		onActivate();
-	
 		super.update(elapsed);
 	}
-
-	private function finishInteraction(name:String):Void
-	{
-		if (name == "interacted"){
-			animation.play("looping",true);	
-		}	
-	}
-
-	override public function onActivate():Void{		
+	
+	public function onActivate():Void{		
 		if(!songLearned)
 			learnSong();
 
-		if (Reg._player.getMandolinObj().getWaterPlayed()){
-			super.onActivate();
-			if(!storyLearned){
-				//Text prompt 2 scrolls, shares story
-				storyLearned = true;
-			}
+		else if (Reg.mando.getWaterPlayed()){		//If water song played
+			if(!storyLearned)
+				learnStory();
 			finishInteraction("interacted");
+			Reg.mando.waterPlayed(false);
 		}
 	}
 
-	/*	@function : Enables the water song
-			if they have not already unlocked it
-			& plays the first set of text
+	private function finishInteraction(name:String):Void{
+		if (name == "interacted")
+			animation.play("looping",true);	
+	}
+
+	/*	@function : Triggers scrolling text to learn song
 	*/
 	private function learnSong(){
-		Reg._player.getMandolinObj().enableWaterSong();
-		ticker = new TickingText("Watershrine_interact.txt");
-		FlxG.sound.play("Water_Song");	}
+		trace("Learning Song");
+		ticker = new TickingText("Watershrine_interact_1.txt");
 		songLearned = true;
+	}
+
+	/*	@function : Triggers the story text
+			and Water Song to play
+	*/
+	private function learnStory(){
+		trace("Learning Story");
+		FlxG.sound.play("Water_Song");
+		Reg.mando.enableWaterSong();
+		animation.play("interacted");
+		//Ticking Text
+		storyLearned = true;
 	}
 
 }
