c28eff323d5da8f411b200213ddb2e3259819fcc
Staethe <jzmynb@gmail.com>
2016-09-18 12:39:05 -0400

    Added Update function & collision helper variables. Started crafting collision logic

--- Project1/source/Shrine.hx
+++ Project1/source/Shrine.hx
@@ -8,6 +8,8 @@ import flixel.FlxSprite;
 class Shrine extends FlxSprite
 {
 
+	private var collisionPlayer:Bool;
+
 	public function new(X:Float, Y:Float, ?Scale:Float = 2) 
 	{
 		super(X, Y);
@@ -18,8 +20,26 @@ class Shrine extends FlxSprite
 		x -= frameWidth*Scale / (2);
 		y -= frameHeight*(Scale)-32;
 	}
+	
+	override public function update(elapsed:Float):Void{
+		//check collision player
+			//setCollisionPlayer
+		super.update(elapsed);
+	}
+
 	private function create(){	}
+	
 	public function onActivate(){
 		animation.play("interacted", false);
 	}
+
+	private function setCollisionPlayer(condition:Bool):Void{
+		collisionPlayer = condition;
+	}
+
+	public function getCollisionPlayer():Bool{
+		return collisionPlayer;
+	}
+
+
 }
