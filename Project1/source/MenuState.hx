8159af1dcdbc98faf655579c847e6397752cf821
Kaitlin Dell <kaitlinadell@gmail.com>
2016-09-20 17:29:23 -0400

    wall on star shrine and new play button

--- Project1/source/MenuState.hx
+++ Project1/source/MenuState.hx
@@ -24,7 +24,7 @@ class MenuState extends FlxState
 		bkg.loadGraphic("assets/images/Menu_bkg.png", false, 640, 480);
 		add(bkg);
 		// add(new FlxText(10,10,20, "Hello, world!"));
-		_playButton  = new FlxButton(0, 0, "Play",function(){FlxG.switchState(new WorldState("water_shrine.tmx")); });
+		_playButton  = new FlxButton(0, 0, "",function(){FlxG.switchState(new WorldState("water_shrine.tmx")); });
 		_playButton.loadGraphic("assets/images/Button_SpriteSheet.png", true, 160, 64);
 		_playButton.screenCenter();
 		_playButton.y += 150;
