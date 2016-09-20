4b72c1ccba38d3b886d712207143acc7298f10af
Staethe <jzmynb@gmail.com>
2016-09-20 01:37:05 -0400

    Fixed ScareCrow text issues

--- Project1/source/ScarecrowShrine.hx
+++ Project1/source/ScarecrowShrine.hx
@@ -47,9 +47,9 @@ class ScarecrowShrine extends Shrine
 		animation.add("looping", [4,3,4,5], 3, true);
 
 		//Initializing Story Text Files
-		ticker = new TickingText(false, "Earthshrine_interact_0.txt", .04, 12, "Water_Text", 100, Std.int(y) - 100);
-		ticker2 = new TickingText(false, "Earthshrine_text_1.txt", .04, 12, "Water_Text", 100, Std.int(y) - 100);
-		ticker3 = new TickingText(false, "Earthshrine_after_song_2.txt", .04, 12, "Water_Text", 100, Std.int(y) - 100);
+		ticker = new TickingText(false, "Earthshrine_interact_0.txt", .04, 12, "Earth_Text", 700, 400);
+		ticker2 = new TickingText(false, "Earthshrine_interact_0.txt", .04, 12, "Earth_Text", 700, 400);
+		ticker3 = new TickingText(false, "Earthshrine_interact_0.txt", .04, 12, "Earth_Text", 700, 400);
 	}
 
 	public function onActivate():Void{
@@ -63,7 +63,6 @@ class ScarecrowShrine extends Shrine
 				ticker3.resetText();
 				WorldState.instance.add(ticker3);
 				ticker3.doSkip = true;
-				ticker3.scrollFactor.set(1,1);
 			}
 			finishInteraction("interacted");
 			Reg.mando.earthPlayed(false);
@@ -83,7 +82,6 @@ class ScarecrowShrine extends Shrine
 		trace("Learning Song");
 		songLearned = true;
 		WorldState.instance.add(ticker);
-		ticker.scrollFactor.set(1,1);
 	}
 
 	/*	@function : Triggers the story text
@@ -96,7 +94,6 @@ class ScarecrowShrine extends Shrine
 		animation.play("interacted");
 		storyLearned = true;
 		WorldState.instance.add(ticker2);
-		ticker2.scrollFactor.set(1, 1);
 		ticker.kill(); //Prevents text overlapping
 	}
 
