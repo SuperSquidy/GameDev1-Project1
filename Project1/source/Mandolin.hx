32ed43e51e81753f6c5f3578d940a83e508b2d13
Staethe <jzmynb@gmail.com>
2016-09-20 21:18:48 -0400

    Changed star song

--- Project1/source/Mandolin.hx
+++ Project1/source/Mandolin.hx
@@ -44,7 +44,7 @@ class Mandolin extends FlxBasic
 	public static var _waterSong:Array<String>	= [";","L"];				//Double Jump
 	public static var _windSong:Array<String> 	= ["J", "J", "J"];			//Dash
 	public static var _earthSong:Array<String>	= ["H", "J", "L", "K", "H"];					//Grow a temporary plant platform
-	public static var _starSong:Array<String> 	= [ "k", "j", "h", "j", ";", ";", "h", "k", "l"];	//Fill the sky with stars
+	public static var _starSong:Array<String> 	= [ "k", "j", "h", "j", "h", "k", "l"];	//Fill the sky with stars
 	
 	//Flag Processing - True if the player has access to the special effects of the song
 	//Changed to public static so that they persist between levels
